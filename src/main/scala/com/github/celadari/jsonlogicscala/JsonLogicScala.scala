0ccdf858a27ea3f1fc26b5ead405e814a69c000d
Charles LADARI <charles-edouard@invenis.co>
2019-04-10 12:44:19 +0200

    More advanced example with generic-typed custom type
    Custom class in the json is a generic class: Person[T](name: String,
    kids: T).

--- src/main/scala/com/github/celadari/jsonlogicscala/JsonLogicScala.scala
+++ src/main/scala/com/github/celadari/jsonlogicscala/JsonLogicScala.scala
@@ -9,18 +9,24 @@ import play.api.libs.json._
 case object JsonLogicScala {
 
   object Person {
-    implicit val personRead: Reads[Person] = Json.reads[Person]
+    implicit def personRead[T](implicit fmt: Reads[T]): Reads[Person[T]] = new Reads[Person[T]] {
+      override def reads(json: JsValue): JsResult[Person[T]] = {
+        val name = (json \ "name").as[String]
+        val kids = (json \ "kids").as[T]
+        JsSuccess(Person(name, kids))
+      }
+    }
   }
 
-  case class Person(name: String, kids: Int)
+  case class Person[T](name: String, kids: T)
 
   def main(args: Array[String]): Unit = {
-    val js = """[{"<": [{"var": "car", "type": "int"}, {"var": "voiture", "type": "long"}, {"var": "jack", "type": "person"}]}, {"jack": {"name": "Jack", "kids": 5}, "car": 2, "voiture": 5}]"""
+    val js = """[{"<": [{"var": "car", "type": "int"}, {"var": "voiture", "type": "long"}, {"var": "jack", "type": "person[Int]"}]}, {"jack": {"name": "Jack", "kids": 5}, "car": 2, "voiture": 5}]"""
 
     implicit val myDecoder: Decoder = new Decoder {
       override def customDecode(json: JsValue, otherType: String)(implicit reads: Array[Reads[_]] = Array()): Any =
         otherType match {
-          case "person" => json.as[Person]
+          case "person[Int]" => json.as[Person[Int]]
           case _ => throw new IllegalArgumentException("Wrong argument.")
         }
     }
